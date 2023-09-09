b2cf225ad4b78fbd9ad517871a682237960c6011
Sadia Thasina Samanta <sadiakvs.724@gmail.com>
2023-09-09 15:12:48 +0400

    Initial Commit

---
+++ GetToKnowMe/GetToKnowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetToKnowMeApp.swift
+//  GetToKnowMe
+//
+//  Created by Sadia Thasina on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetToKnowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
